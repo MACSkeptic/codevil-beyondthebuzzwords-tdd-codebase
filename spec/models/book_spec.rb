0bcdbb08c1e581f5657e9898f6c9ddd65b6bcc5b
macskeptic <macskeptic@gmail.com>
2009-07-26 21:09:20 -0300

    created the tests to ensure that title and author are both mandatory

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe Book do
+describe Book, "with all attributes properly set" do
   before(:each) do
     @valid_attributes = {
       :title => "value for title",
@@ -11,6 +11,52 @@ describe Book do
   end
 
   it "should create a new instance given valid attributes" do
-    Book.create!(@valid_attributes)
+    lambda { Book.create!(@valid_attributes) }.should_not raise_error
   end
 end
+
+describe Book, "missing rating and synopsis" do
+  before(:each) do
+    @valid_attributes = {
+      :title => "value for title",
+      :author => "value for author"
+    }
+  end
+
+  it "should be OK, since rating and synopsis are optional" do
+    lambda { Book.create!(@valid_attributes) }.should_not raise_error
+  end
+end
+
+describe Book, "missing author" do
+  before(:each) do
+    @invalid_attributes = {
+      :title => "value for title",
+      :rating => 9.99,
+      :synopsis => "value for synopsis"
+    }
+  end
+
+  it "should not be allowed, because author is mandatory" do
+    # lambda { Book.create!(@invalid_attributes) }.should raise_error <= this is another way to do it ;)
+    book = Book.create(@invalid_attributes)
+    book.errors_on(:author).should_not be_empty
+  end
+end
+
+describe Book, "missing title" do
+  before(:each) do
+    @invalid_attributes = {
+      :author => "value for author",
+      :rating => 9.99,
+      :synopsis => "value for synopsis"
+    }
+  end
+
+  it "should not be allowed, because title is mandatory" do
+    # lambda { Book.create!(@invalid_attributes) }.should raise_error  <= this is another way to do it ;)
+    book = Book.create(@invalid_attributes)
+    book.errors_on(:title).should_not be_empty
+  end
+end
+
