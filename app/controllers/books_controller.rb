9542c2c2f60ccbd37e3956a405601b8f6b33a2e4
macskeptic <macskeptic@gmail.com>
2009-07-26 22:41:19 -0300

    tests passing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
   def create
     @book = Book.new params[:book]
+    @book.save!
+    flash[:notice] = "The book was saved successfully"
     render :text => ""
   end
 end
