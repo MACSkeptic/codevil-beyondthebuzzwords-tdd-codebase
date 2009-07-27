a54105c486d1fb65a713e7b90c5f7bfea562bfdd
macskeptic <macskeptic@gmail.com>
2009-07-26 22:21:37 -0300

    added the test to check if the constructor of Book is called

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -6,3 +6,20 @@ describe BooksController do
   end
 end
 
+describe BooksController, "creating a valid book" do
+  before(:each) do
+    @params_for_save = { "title" => "advanced joseki", "author" => "honinbou shusaku" }
+    @book = mock_model(Book, :save! => true)
+    Book.stub!(:new).and_return(@book)
+  end
+
+  def call_create
+    post :create, { :book => @params_for_save }
+  end
+
+  it "should instantiate a new book with the given parameters" do
+    Book.should_receive(:new).with(@params_for_save).and_return(@book)
+    call_create
+  end
+end
+
