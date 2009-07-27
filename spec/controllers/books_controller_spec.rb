1f83343ec79d4318c041b2a5a037acd5e8609191
macskeptic <macskeptic@gmail.com>
2009-07-26 21:34:43 -0300

    test for the books controller to respond to create

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,10 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe BooksController do
-
-  #Delete this example and add some real ones
-  it "should use BooksController" do
-    controller.should be_an_instance_of(BooksController)
+  it "should have an action that allow the creation of new books" do
+    controller.should respond_to(:create)
   end
-
 end
+
